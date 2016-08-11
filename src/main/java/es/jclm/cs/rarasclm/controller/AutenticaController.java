ae2c9ab7b8540ca4d941fe280670182af4aa13d3
Ricardo Ortega Galiana <Ricardo Ortega Galiana>
2016-08-11 07:48:24 +0200

    java7 Datasource con jndi

--- src/main/java/es/jclm/cs/rarasclm/controller/AutenticaController.java
+++ src/main/java/es/jclm/cs/rarasclm/controller/AutenticaController.java
@@ -6,17 +6,10 @@ package es.jclm.cs.rarasclm.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.hibernate.annotations.AttributeAccessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 // TODO: Auto-generated Javadoc
